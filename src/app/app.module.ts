0e5e804889520437ff0413d319bf3862c2b53a4f
Arshith Devanand <arshithdev07@gmail.com>
2019-09-24 18:14:51 -0500

    Filter dropdown to filter crimes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,9 @@ import { CrimesComponent } from './crimes/crimes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/crimes/crimes.component.ts
+++ src/app/crimes/crimes.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-crimes',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrimesComponent implements OnInit {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+  districts: any;
+  years = ['2019', '2018', '2017', '2016', '2015'];
+  selectedDistrict: {
+    districtNo: string,
+    districtName: string
+  };
+  selectedYear = '2019';
+
+  constructor(private http : HttpClient) { 
+    console.log(environment.apiUrl);
+  }
 
   ngOnInit() {
+    this.getDistricts();
+  }
+
+  getDistricts() {
+      this.http.get(`${this.apiUrl}/districts`)
+      .subscribe(districts => {
+        console.log(districts);
+        this.districts = districts;
+        this.selectedDistrict = districts[0];
+      });
+  }
+
+  changeDistrict(event, newDisctrict) {
+    this.selectedDistrict = newDisctrict;
+  }
+
+  changeYear(event, newYear) {
+    this.selectedYear = newYear;
+  }
+
+  filterRecords() {
+    let date = this.selectedYear + "-01-01";
+    this.http.get(`${this.apiUrl}/filteredCrimes?crimeDate=${date}&districtNo=${this.selectedDistrict.districtNo}`)
+    .subscribe(crimes => {
+      console.log("filtered crimes",crimes);
+    });
   }
 
 }
