545092d5fb2f4d5bb9816e2dc52da2c65b21927a
Arshith Devanand <arshithdev07@gmail.com>
2019-11-13 01:48:40 -0600

    Mongo Implementation of recommended districts feature

--- src/app/crimes/crimes.component.ts
+++ src/app/crimes/crimes.component.ts
@@ -13,7 +13,7 @@ export class CrimesComponent implements OnInit {
 
   apiUrl = environment.apiUrl;
   districts: any;
-  years = ['2019', '2018', '2017', '2016', '2015'];
+  years = ['2019', '2018'];
   selectedDistrict: {
     districtNo: string,
     districtName: string
@@ -52,34 +52,34 @@ export class CrimesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getDistricts();
+    // this.getDistricts();
     this.groupCrimesByDistrict();
   }
 
-  getDistricts() {
-      this.http.get(`${this.apiUrl}/districts`)
-      .subscribe(districts => {
-        console.log(districts);
-        this.districts = districts;
-        this.selectedDistrict = districts[0];
-      });
-  }
+  // getDistricts() {
+  //     this.http.get(`${this.apiUrl}/districts`)
+  //     .subscribe(districts => {
+  //       console.log(districts);
+  //       this.districts = districts;
+  //       this.selectedDistrict = districts[0];
+  //     });
+  // }
 
-  changeDistrict(event, newDisctrict) {
-    this.selectedDistrict = newDisctrict;
-  }
+  // changeDistrict(event, newDisctrict) {
+  //   this.selectedDistrict = newDisctrict;
+  // }
 
   changeYear(event, newYear) {
     this.selectedYear = newYear;
   }
 
-  filterRecordsByDistrictAndYear() {
-    let date = this.selectedYear + "-01-01";
-    this.http.get(`${this.apiUrl}/filteredCrimes?crimeDate=${date}&districtNo=${this.selectedDistrict.districtNo}`)
-    .subscribe(crimes => {
-      console.log("filtered crimes",crimes);
-    });
-  }
+  // filterRecordsByDistrictAndYear() {
+  //   let date = this.selectedYear + "-01-01";
+  //   this.http.get(`${this.apiUrl}/filteredCrimes?crimeDate=${date}&districtNo=${this.selectedDistrict.districtNo}`)
+  //   .subscribe(crimes => {
+  //     console.log("filtered crimes",crimes);
+  //   });
+  // }
 
   groupCrimesByDistrict() {
     let date = this.selectedYear + "-01-01";
@@ -88,8 +88,8 @@ export class CrimesComponent implements OnInit {
       console.log("Crime Count ",crimesCount);
       const items: Object[] = <Object[]>crimesCount;
       
-      var districtArray: string[] = items.map(item => item['label']);
-      const numberList: number[] = items.map(item => item['data']);
+      var districtArray: string[] = items.map(item => item['districtName']);
+      const numberList: number[] = items.map(item => item['count']);
       
       console.log(districtArray);
       console.log(numberList);
